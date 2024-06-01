8b0face6e6429126445fcb8db9502e9003179958
codemile <nick@reactgular.com>
2024-06-01 17:18:31 -0400

    chore: Remove deprecated code and environment variables
    This commit removes deprecated code and environment variables that are no longer used in the application. The `.env`, `.env.local`, and `.env.production` files have been updated to remove the `NEXT_PUBLIC_BASE` and `NEXT_PUBLIC_ANALYTICS` variables. Additionally, the `inter` font import in the `layout.tsx` file has been removed, as it is no longer needed. The `useTitle` hook in the `useTitle.tsx` file has been marked as deprecated and a comment has been added to suggest using Next.js metadata instead. Finally, the `base` property in the `environment.ts` file has been removed, as it is no longer used.

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,30 +1,16 @@
-const endsInSlash = (str: string): string => str.endsWith('/') ? str : `${str}/`;
-
 export interface Environment {
     analytics: string;
-
-    /**
-     * @deprecated use basePath from router instead
-     */
-    base: string;
 
     brandName: string;
 
     github: string;
 
     storageKey: string;
-
-    /**
-     * @deprecated
-     */
-    version: string;
 }
 
 export const environment: Environment = {
     analytics: process.env.NEXT_PUBLIC_ANALYTICS as string,
     brandName: process.env.NEXT_PUBLIC_BRAND_NAME as string,
     github: process.env.NEXT_PUBLIC_GITHUB as string,
-    storageKey: process.env.NEXT_PUBLIC_STORAGE_KEY as string,
-    version: process.env.NEXT_PUBLIC_VERSION as string,
-    base: endsInSlash(process.env.NEXT_PUBLIC_BASE as string ?? '/')
+    storageKey: process.env.NEXT_PUBLIC_STORAGE_KEY as string
 };
